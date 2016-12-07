142e108f757c609ca1cd550ad4491fed23955891
Gustavo Lizarraga <lizarraga.gux@gmail.com>
2016-12-07 01:17:50 -0400

    Pantalla completa

--- app/src/main/java/com/miramicodigo/studyjam_ii_views_ii/MainActivity.java
+++ app/src/main/java/com/miramicodigo/studyjam_ii_views_ii/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
     }
 }
